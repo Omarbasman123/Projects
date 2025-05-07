322463696494e15304a5699acf0f36c98e17861a
Omar Basman <oalmo045@uottawa.ca>
2025-05-06 23:34:41 -0400

    Create GameState.java

---
+++ GameState.java
@@ -0,0 +1,10 @@
+// NOM: OMAR BASMAN ALMOGHLY
+
+package Devoir2_300406240.Partie1;
+
+public enum GameState {
+    PLAYING, // ce jeu est en cours
+    DRAW,  // ce jeu est un match nul
+    XWIN,  // ce jeu a été gagné par le premier joueur X
+    OWIN  // ce jeu a été gagné par le premier joueur O
+}
