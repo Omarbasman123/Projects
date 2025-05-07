2fd028bb4d69bcb2829031d252afb61ca30c11ca
Omar Basman <oalmo045@uottawa.ca>
2025-05-06 23:32:31 -0400

    BoxSymbol.java

---
+++ BoxSymbol.java
@@ -0,0 +1,9 @@
+//NOM : OMAR BASMAN ALMOGHLY
+
+package Devoir2_300406240.Partie1;
+
+public enum BoxSymbol {
+    EMPTY,  // la cellule est vide
+    X,   //  il y a un X dans la cellule
+    O  // il y a un O dans la cellule
+   }
